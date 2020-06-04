ec84841451b382a23a3eb2074877ebc7cfa7ee16
xiuzhu-github <952871053@qq.com>
2020-06-04 18:09:51 +0800

    1.解决了注册中心服务启动失败的问题 2.优化启动日志提示

--- eureka/src/main/java/com/studyonline/EurekaApplication.java
+++ eureka/src/main/java/com/studyonline/EurekaApplication.java
@@ -1,13 +1,29 @@
 package com.studyonline;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+	/*springboot默认集成sIf4j框架，它定义了一些日志接口，规范日志输出，真正的实现是logback或log4j。
+	我们代码中写的都是slf4j的代码。NP%*/
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+//	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
