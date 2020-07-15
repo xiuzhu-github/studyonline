40fbcbc668d88deba039f07dd8b8fdc2a4e5a7ea
xiuzhu-github <952871053@qq.com>
2020-07-15 20:43:26 +0800

    1.增加eureka子模块作为服务器端 2.将单maven项目改为父子模块

---
+++ eureka/src/main/java/com/studyonline/EurkeaApplication.java
@@ -0,0 +1,13 @@
+package com.studyonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurkeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurkeaApplication.class, args);
+    }
+
+}
