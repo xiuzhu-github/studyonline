dd91142859ab4c28e94399f5d7309878b1b23853
xiuzhu-github <952871053@qq.com>
2020-07-15 20:18:08 +0800

    初始化项目

---
+++ src/main/java/com/example/studyonline/StudyonlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyonline/StudyonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
