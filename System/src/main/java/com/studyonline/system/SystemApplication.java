b9e38545cd7a7cee6c7d31911e78f70f0b41b001
xiuzhu-github <952871053@qq.com>
2020-08-11 20:31:46 +0800

    1.将system注册到eureka

--- System/src/main/java/com/studyonline/system/SystemApplication.java
+++ System/src/main/java/com/studyonline/system/SystemApplication.java
@@ -5,10 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class SystemApplication {
 	/*springboot默认集成sIf4j框架，它定义了一些日志接口，规范日志输出，真正的实现是logback或log4j。
 	我们代码中写的都是slf4j的代码。NP%*/
